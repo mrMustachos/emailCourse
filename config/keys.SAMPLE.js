f81873c7f081e2a04c2e815bfd074812b3fe2051
mrMustachos <bill.domanick@crowdsurge.com>
2018-02-12 14:24:17 -0500

    google oauth start test

---
+++ config/keys.SAMPLE.js
@@ -0,0 +1,4 @@
+module.exports = {
+	googleClientID: 'GOOGLE_CLIENT_ID_HERE',
+	googleClientSecret: 'GOOGLE_CLIENT_SECRET_HERE'
+};
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-// this whole block of code is a route handler 
-app.get('/', (req, res) => {
-	res.send({ bye: 'buddy' });
-});
+// Commom PassportJS Issues:
+//  - Does require us to reach into spcific points of the flow to make stuff work
+//  - inharent confusion on how the library is constructed
+//     • you always need passport [http://www.passportjs.org/docs/]
+//     • you also need the libs for the type of auth you are trying to do ie fb, google, email/password, ect [http://www.passportjs.org/packages/]
+
+passport.use(new googleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
